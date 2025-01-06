f0107b2802f7d468e50f97916069899a79dc2dc6
paucean_ioana <166832687+ioana333@users.noreply.github.com>
2025-01-06 03:25:10 +0200

    1.0

---
+++ snake/src/Main.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main extends Application
+{
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = WIDTH;
+    private static final int ROWS = 20;
+    private static final int COLUMNS = ROWS;
+    private static final int SQUARE_SIZE = WIDTH/ROWS;
+    private static final String[] FOODS_IMAGE = new String[]{};
+
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
